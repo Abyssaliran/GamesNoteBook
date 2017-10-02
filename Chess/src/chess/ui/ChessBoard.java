4d2e6467c618530fd28a95143d4928699aaab937
V.Y.Romanov <vladimir.romanov@gmail.com>
2017-10-02 21:04:40 +0400

    Выделили метод рисования клетки

--- Chess/src/chess/ui/ChessBoard.java
+++ Chess/src/chess/ui/ChessBoard.java
@@ -31,18 +31,23 @@ public class ChessBoard extends Canvas implements PaintListener {
 		GC gc = e.gc;
 		for (int v = 0; v < nV; v++) {
 			for (int h = 0; h < nH; h++) {
-				boolean isWhiteSquare = ((v + h) % 2 == 0);
-				Color squareColor = isWhiteSquare ? new Color(null, 255, 255, 255) 
-				                         : new Color(null,   0, 192,   0);
-				
-			     gc.setBackground(squareColor);
-			     gc.fillRectangle(v * squareWidth, h * squareHeight, squareWidth, squareHeight);
-
-			     gc.setForeground(new Color(null, 0, 0, 0));
-			     gc.drawRectangle(v * squareWidth, h * squareHeight, squareWidth, squareHeight);
+				drawSquare(squareWidth, squareHeight, gc, v, h);
 			}
 		}
 
 		e.gc.drawRectangle(0, 0, clientArea.width - 1, clientArea.height - 1);
+	}
+
+	private void drawSquare(int squareWidth, int squareHeight, GC gc, int v, int h) {
+		boolean isWhiteSquare = ((v + h) % 2 == 0);
+		Color squareColor = isWhiteSquare 
+				                 ? new Color(null, 255, 255, 255) 
+		                         : new Color(null,   0, 192,   0);
+		
+		 gc.setBackground(squareColor);
+		 gc.fillRectangle(v * squareWidth, h * squareHeight, squareWidth, squareHeight);
+
+		 gc.setForeground(new Color(null, 0, 0, 0));
+		 gc.drawRectangle(v * squareWidth, h * squareHeight, squareWidth, squareHeight);
 	}
 }
