9246df536f7d32e25dcc25ed08fe7c0778a0b86a
V.Y.Romanov <vladimir.romanov@gmail.com>
2017-10-02 21:10:29 +0400

    Создали базовый класс для досок всех игр.

--- Chess/src/chess/ui/ChessBoard.java
+++ Chess/src/chess/ui/ChessBoard.java
@@ -1,44 +1,21 @@
 package chess.ui;
 
-import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 /**
  * @author Romanov_V_Y Шахматная доска.
  */
-public class ChessBoard extends Canvas implements PaintListener {
-	private int nV = 8;
-	private int nH = 8;
-
+public class ChessBoard extends GameBoard {
 	public ChessBoard(Composite composite, int style) {
 		super(composite, style);
 
 		addPaintListener(this);
 	}
 
-	@Override
-	public void paintControl(PaintEvent e) {
-		Rectangle clientArea = getClientArea();
-
-		int squareWidth = getClientArea().width / nV;
-		int squareHeight = getClientArea().height / nH;
-
-		GC gc = e.gc;
-		for (int v = 0; v < nV; v++) {
-			for (int h = 0; h < nH; h++) {
-				drawSquare(squareWidth, squareHeight, gc, v, h);
-			}
-		}
-
-		e.gc.drawRectangle(0, 0, clientArea.width - 1, clientArea.height - 1);
-	}
-
-	private void drawSquare(int squareWidth, int squareHeight, GC gc, int v, int h) {
+	void drawSquare(int squareWidth, int squareHeight, GC gc, int v, int h) {
 		boolean isWhiteSquare = ((v + h) % 2 == 0);
 		Color squareColor = isWhiteSquare 
 				                 ? new Color(null, 255, 255, 255) 
---
+++ Chess/src/chess/ui/GameBoard.java
@@ -0,0 +1,36 @@
+package chess.ui;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+public abstract class GameBoard extends Canvas implements PaintListener  {
+
+	private int nV = 8;
+	private int nH = 8;
+
+	public GameBoard(Composite parent, int style) {
+		super(parent, style);
+	}
+
+	@Override
+	public void paintControl(PaintEvent e) {
+		Rectangle clientArea = getClientArea();
+	
+		int squareWidth = getClientArea().width / nV;
+		int squareHeight = getClientArea().height / nH;
+	
+		GC gc = e.gc;
+		for (int v = 0; v < nV; v++) {
+			for (int h = 0; h < nH; h++)  
+				drawSquare(squareWidth, squareHeight, gc, v, h);
+		}
+	
+		e.gc.drawRectangle(0, 0, clientArea.width - 1, clientArea.height - 1);
+	}
+
+	abstract void drawSquare(int squareWidth, int squareHeight, GC gc, int v, int h);
+}
