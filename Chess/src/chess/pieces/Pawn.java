7235369edeafe76866a11c107ce7dd8224cc035e
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-13 23:55:46 +0300

    Создали класс для пешки в европейских шахматах.

---
+++ Chess/src/chess/pieces/Pawn.java
@@ -0,0 +1,31 @@
+package chess.pieces;
+
+import game.core.Move;
+import game.core.Square;
+
+/**
+ * Класс представляющий на доске пешку европейских шахмат.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class Pawn extends ChessPiece {
+	public Pawn(Square square) {
+		super(square);
+	}
+
+	@Override
+	public boolean isCorrectMove(Square... squares) {
+		// Пока используем только умалчиваемую проверку
+		// выполняемую в базовом классе.
+		if (!super.isCorrectMove(squares))
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public Move makeMove(Square... squares) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
