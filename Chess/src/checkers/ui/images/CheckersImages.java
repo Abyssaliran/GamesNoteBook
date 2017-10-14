697c7bb891c0e2a24abea98d2fbb9e034274f322
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-14 14:55:47 +0300

    Добавлены классы и файлы для работы с изображениями шашек.

---
+++ Chess/src/checkers/ui/images/CheckersImages.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package checkers.ui.images;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * Изображения для игры в шашки.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class CheckersImages {
+	public static Image iconCheсkers;
+	
+	public static Image imageManWhite;
+	public static Image imageKingWhite;
+
+	public static Image imageManBlack;
+	public static Image imageKingBlack;
+
+	static {
+		load( Display.getCurrent() );
+	}
+
+	private static void load(final Display display) {
+		iconCheсkers    = new Image(display, CheckersImages.class.getResourceAsStream("iconCheсkers.png"));
+
+		imageKingBlack  = new Image(display, CheckersImages.class.getResourceAsStream("bKing.png"));
+		imageManBlack   = new Image(display, CheckersImages.class.getResourceAsStream("bMan.png"));
+		
+		imageKingWhite  = new Image(display, CheckersImages.class.getResourceAsStream("wKing.png"));
+		imageManWhite   = new Image(display, CheckersImages.class.getResourceAsStream("wMan.png"));
+	}
+}
