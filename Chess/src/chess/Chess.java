78135906e7bbd529541a07d27989f3ee41925d6c
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-14 11:45:24 +0300

    Расставили все шахматные фигуры на доске.

--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -1,9 +1,19 @@
 package chess;
 
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
+import chess.pieces.Rook;
 import game.core.Board;
 import game.core.PieceColor;
 
+/**
+ * Класс представляющий игру шахматы.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
 public class Chess {
 	/**
 	 * Расстановка шахматных фигур в начальную позицию.
@@ -11,10 +21,39 @@ public class Chess {
 	 */
 	static public Board getInitBoard() {
 		Board board = new Board(8, 8);
+		
+		// Расставляем пешки.
 		for (int v = 0; v < board.nV; v++) {
 			new Pawn(board.getSquare(v, 1), PieceColor.BLACK);
 			new Pawn(board.getSquare(v, 6), PieceColor.WHITE);
 		}
+
+		// Расставляем ладьи.
+		new Rook(board.getSquare(0, 0), PieceColor.BLACK);
+		new Rook(board.getSquare(7, 0), PieceColor.BLACK);
+		new Rook(board.getSquare(0, 7), PieceColor.WHITE);
+		new Rook(board.getSquare(7, 7), PieceColor.WHITE);
+
+		// Расставляем ладьи.
+		new Knight(board.getSquare(1, 0), PieceColor.BLACK);
+		new Knight(board.getSquare(6, 0), PieceColor.BLACK);
+		new Knight(board.getSquare(1, 7), PieceColor.WHITE);
+		new Knight(board.getSquare(6, 7), PieceColor.WHITE);
+
+		// Расставляем слонов.
+		new Bishop(board.getSquare(2, 0), PieceColor.BLACK);
+		new Bishop(board.getSquare(5, 0), PieceColor.BLACK);
+		new Bishop(board.getSquare(2, 7), PieceColor.WHITE);
+		new Bishop(board.getSquare(5, 7), PieceColor.WHITE);
+
+		// Расставляем ферзей.
+		new Queen(board.getSquare(3, 0), PieceColor.BLACK);
+		new Queen(board.getSquare(3, 7), PieceColor.WHITE);
+
+		// Расставляем королей.
+		new King(board.getSquare(4, 0), PieceColor.BLACK);
+		new King(board.getSquare(4, 7), PieceColor.WHITE);
+
 		return board;
 	}
 }
