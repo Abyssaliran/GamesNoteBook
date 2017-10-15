13b5aaa0f1da29a9578b33f23658dd0c7f4b13be
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-15 10:38:28 +0300

    Добавлены изображения общие для всех игр.
    Добавлена текстура дерева.

---
+++ Chess/src/game/ui/images/GameImages.java
@@ -0,0 +1,25 @@
+package game.ui.images;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * Класс для доступа к изображениям общим для всех игр.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class GameImages {
+	public static Image woodDark;
+	public static Image woodLight;
+	public static Image woodMedium;
+
+	static {
+		load( Display.getCurrent() );
+	}
+
+	private static void load(final Display display) {
+		woodDark   = new Image(display, GameImages.class.getResourceAsStream("wood_dark.png"));
+		woodLight  = new Image(display, GameImages.class.getResourceAsStream("wood_light.png"));
+		woodMedium = new Image(display, GameImages.class.getResourceAsStream("wood_medium.png"));
+	}  
+} 
