019502bd83c0ba3258c71dc2c12e975456267cd5
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-20 16:24:27 +0300

    Созданы пакеты и классы для игры Реверси.

---
+++ Chess/src/reversi/pieces/Stone.java
@@ -0,0 +1,53 @@
+package reversi.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import game.core.Move;
+import game.core.Piece;
+import game.core.PieceColor;
+import game.core.Square;
+import reversi.moves.ReversiMove;
+
+/**
+ * Фигура-камень 
+ * для <a href="https://ru.wikipedia.org/wiki/%D0%A0%D0%B5%D0%B2%D0%B5%D1%80%D1%81%D0%B8">Реверси</a>
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class Stone extends Piece {
+
+	public Stone(Square square, PieceColor color) {
+		super(square, color);
+	}
+
+	@Override
+	public boolean isCorrectMove(Square... squares) {
+		return true;
+	}
+
+	@Override
+	public Move makeMove(Square... squares) {
+		Square target = squares[0];
+		
+		List<Square> captured = collectCaptured(target);
+		
+		return new ReversiMove(target, captured);
+	}
+
+	/**
+	 * Собрать все клетки, на которых стоят захваченные в плен (окруженные)
+	 * фигуры противника.
+	 * 
+	 * @param target - клетка куда поставлена фигура.
+	 * @return - клетки с захваченными в плен фигурами противника.
+	 */
+	private List<Square> collectCaptured(Square target) {
+		List<Square> captured = new ArrayList<>();
+		
+		// TODO Auto-generated method stub
+		
+		return captured;
+	}
+
+}
