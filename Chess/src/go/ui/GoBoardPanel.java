d68409306d90faea863315c2e8c92d95a851de23
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-21 20:59:06 +0300

    Реализован диалог с игрой Го.

--- Chess/src/go/ui/GoBoardPanel.java
+++ Chess/src/go/ui/GoBoardPanel.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package go.ui;
 
 import org.eclipse.swt.graphics.Image;
@@ -8,10 +5,12 @@ import org.eclipse.swt.widgets.Composite;
 
 import game.core.Piece;
 import game.core.PieceColor;
+import game.core.Square;
 import game.ui.AsiaBoard;
-
+import game.ui.listners.PutPieceListener;
 import go.Go;
 import go.ui.images.GoImages;
+import reversi.pieces.Stone;
 
 /**
  * Панель для игры <a href="https://ru.wikipedia.org/wiki/%D0%93%D0%BE">Го</a>
@@ -21,6 +20,24 @@ import go.ui.images.GoImages;
 public class GoBoardPanel extends AsiaBoard {
 	public GoBoardPanel(Composite parent, int boardSize) {
 		super(parent, Go.getInitBoard(boardSize, boardSize));
+		
+		listner = new PutPieceListener(this) {
+			@Override
+			public Image getPieceImage(Piece piece, PieceColor color) {
+				return GoBoardPanel.this.getPieceImage(piece, color);
+			}
+
+			@Override
+			public Piece getPiece(Square square, PieceColor color) {
+				return new Stone(square, color);
+			}
+		};
+	}
+
+	protected Image getPieceImage(Piece piece, PieceColor color) {
+		return color == PieceColor.WHITE 
+				? GoImages.imageStoneWhite
+				: GoImages.imageStoneBlack;
 	}
 
 	/*
@@ -30,8 +47,6 @@ public class GoBoardPanel extends AsiaBoard {
 	 */
 	@Override
 	public Image getPieceImage(Piece piece) {
-		return piece.getColor() == PieceColor.WHITE 
-				? GoImages.imageStoneWhite
-				: GoImages.imageStoneBlack;
+		return getPieceImage(piece, piece.getColor());
 	}
 }
