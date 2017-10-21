feab3b8c7cbb125759f9f3ff3afbcce10718c64e
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-21 22:41:55 +0300

    Добавлены конструкторы в классы - шахматные ходы.

--- Chess/src/chess/moves/Capture.java
+++ Chess/src/chess/moves/Capture.java
@@ -1,5 +1,7 @@
 package chess.moves;
 
+import game.core.Square;
+
 /**
  * Ход европейских шахмат - перемещение фигуры на клетку 
  * с захватом фигуры противника.
@@ -7,6 +9,10 @@ package chess.moves;
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
 public class Capture extends SimpleMove implements ICapture {
+	public Capture(Square[] squares) {
+		super(squares);
+	}
+
 	@Override
 	public void doMove() {
 		// TODO Auto-generated method stub
