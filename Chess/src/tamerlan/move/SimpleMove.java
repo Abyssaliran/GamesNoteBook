02a0324af10a2ae9ff3c58dc832b4b559e4f4aa5
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-21 23:57:48 +0300

    Реализовано упрощенное перемещение фигур в шахматах Тамерлана.

--- Chess/src/tamerlan/move/SimpleMove.java
+++ Chess/src/tamerlan/move/SimpleMove.java
@@ -1,21 +1,44 @@
 package tamerlan.move;
 
 import game.core.Move;
+import game.core.Piece;
+import game.core.Square;
 
 /**
- * Простой ход - перемещение фигуры на пустую клетку.
+ * Простой ход шахмат тамерлана - перемещение фигуры на пустую клетку.
  *  
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
 public class SimpleMove implements Move {
+	/**
+	 * Какая фигура перемещается.
+	 */
+	private Piece piece;
+	
+	/**
+	 * Откуда перемещается.
+	 */
+	private Square source;
+	
+	/**
+	 * Куда перемещается.
+	 */
+	private Square target;
+
+	public SimpleMove(Square[] squares) {
+		source = squares[0];
+		target = squares[1];
+		
+		piece = source.getPiece();
+	}
 
 	@Override
 	public void doMove() {
-		// TODO Auto-generated method stub
+		piece.moveTo(target);
 	}
 
 	@Override
 	public void undoMove() {
-		// TODO Auto-generated method stub
+		piece.moveTo(source);
 	}
 }
