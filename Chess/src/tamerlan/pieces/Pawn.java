104ef53912af8857502a0211dc42d3f299ff9b0f
avazutaev <avazutaev@gmail.com>
2017-10-25 07:40:10 +0300

    Проверка правильности хода пешкой

--- Chess/src/tamerlan/pieces/Pawn.java
+++ Chess/src/tamerlan/pieces/Pawn.java
@@ -2,8 +2,10 @@ package tamerlan.pieces;
 
 import chess.moves.SimpleMove;
 import game.core.Move;
+import game.core.Piece;
 import game.core.PieceColor;
 import game.core.Square;
+import tamerlan.move.Capture;
 
 /**
  * Класс представляющий на доске пешку европейских шахмат.
@@ -24,6 +26,33 @@ public class Pawn extends TamerlanPiece {
 		
 		// TODO Утаев - проверить правильность хода пешкой.
 		
+		//Пешка не может делать первый ход на два поля. 
+		//Соответственно, отсутствует и взятие на проходе
+		Square target = squares[0];
+		if (Math.abs(target.h - this.square.h) > 1) {
+			return false;
+		}
+		//Пешка не может делать ход по диагонали,если это не захват
+		if (this.square.h != target.h && this.square.v != target.v && target.isEmpty()) {
+			return false;
+		}
+		//Пешка не может делать ход на занятую клетку впереди
+		if (this.square.v == target.v && !target.isEmpty()) {
+			return false;
+		}
+		//Пешка не может делать ход по текущей горизонтали
+		if (target.h == this.square.h) {
+			return false;
+		}
+		//Пешка не может делать ход назад (правило для белой фигуры)
+		if (this.getColor() == PieceColor.WHITE && target.h > this.square.h) {
+			return false;
+		}
+		//Пешка не может делать ход назад (правило для черной фигуры)
+		if (this.getColor() == PieceColor.BLACK && target.h < this.square.h) {
+			return false;
+		}
+		
 		return true;
 	}
 
@@ -32,6 +61,12 @@ public class Pawn extends TamerlanPiece {
 		// TODO Утаев - если это захват фигуры противника,
 		// то вернуть ход-захват фигуры new Capture();
 		
-		return new SimpleMove(squares);
+		Square target = squares[1];
+		
+		if (!target.isEmpty()) {
+			return new Capture(squares);
+		} else {
+			return new SimpleMove(squares);
+		}
 	}
 }
