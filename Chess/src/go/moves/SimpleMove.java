fc3123219b423aeb82aa610ecb833154eb9f2d79
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-10-29 13:25:38 +0300

    Пока только простой ход в Го.

--- Chess/src/go/moves/SimpleMove.java
+++ Chess/src/go/moves/SimpleMove.java
@@ -1,13 +1,19 @@
 package go.moves;
 
 import game.core.Move;
+import game.core.Square;
 
 /**
- * Ход без захвата фигуры противника для <a href="https://ru.wikipedia.org/wiki/%D0%93%D0%BE">Го</a>.
+ * Ход без захвата фигуры противника 
+ * для <a href="https://ru.wikipedia.org/wiki/%D0%93%D0%BE">Го</a>.
  * 
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
 public class SimpleMove implements Move {
+	public SimpleMove(Square... squares) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void doMove() {
 		// TODO Auto-generated method stub
