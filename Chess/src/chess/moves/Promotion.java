70d6ea1af5570cb9d4a4cf761870cb190596e3ca
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-11-06 18:11:11 +0300

    Chess. Реализованно превращение пешки.

--- Chess/src/chess/moves/Promotion.java
+++ Chess/src/chess/moves/Promotion.java
@@ -1,5 +1,7 @@
 package chess.moves;
 
+import chess.pieces.Queen;
+import game.core.Piece;
 import game.core.Square;
 
 /**
@@ -8,31 +10,53 @@ import game.core.Square;
  *  
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
-public class Promotion extends SimpleMove implements ICapture {
+public class Promotion extends SimpleMove {
+
+	private Piece capturedPiece;
+	private Queen promotedPiece;
+	private Piece pawn;
 
 	public Promotion(Square[] squares) {
 		super(squares);
+		
+		pawn = source.getPiece();
+		
+		if (source.v != target.v)  
+			// Ход по диагонали со взятием фигуры.
+			capturedPiece = target.getPiece();
 	}
-
+	
+	/* 
+	 * Удалить пешку, поставить фигуру.
+	 */
 	@Override
 	public void doMove() {
-		// TODO Auto-generated method stub
+		if (capturedPiece != null)
+			target.removePiece();
+
+		source.removePiece();
+		promotedPiece = new Queen(target, pawn.getColor());
+		
+		target.setPiece(promotedPiece);
 	}
 
+	/* 
+	 * Удалить фигуру, поставить пешку.
+	 */
 	@Override
 	public void undoMove() {
-		// TODO Auto-generated method stub
-	}
+		target.removePiece();
+		source.setPiece(piece);
 
-	@Override
-	public void removePiece() {
-		// TODO Auto-generated method stub
-		
+		if (capturedPiece != null)
+			target.setPiece(capturedPiece);
 	}
-
+	
 	@Override
-	public void restotePiece() {
-		// TODO Auto-generated method stub
+	public String toString() {
+		String movekind  = (capturedPiece == null) ? "-" : "x";
+		String pieceKind = promotedPiece.toString();
 		
+		return "" + piece + source + movekind + target + pieceKind;
 	}
 }
