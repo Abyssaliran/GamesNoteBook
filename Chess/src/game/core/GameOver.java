f02ce7fea90cff895e651b56fb260241808e4b12
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-11-12 14:50:49 +0300

    games. Комментарии к ситуации - окончанию игры.

--- Chess/src/game/core/GameOver.java
+++ Chess/src/game/core/GameOver.java
@@ -1,7 +1,8 @@
 package game.core;
 
 /**
- * Ситуация окончания игры.
+ * Ситуация окончания игры. Параметр ситуации - результат игры
+ * (переменная типа GameResult)
  * 
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
@@ -13,6 +14,13 @@ public class GameOver extends Exception {
 	
 	public GameResult result;
 	
+	/**
+	 * Ситуация с результатом игры.
+	 * 
+	 * @see GameResult
+	 * 
+	 * @param result результатом игры.
+	 */
 	public GameOver(GameResult result) {
 		this.result = result;
 	}
