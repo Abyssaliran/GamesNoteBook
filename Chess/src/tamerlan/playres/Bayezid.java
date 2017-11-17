a199d9b56d4aaac41f62d62714be0ab8e22e4819
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-11-17 22:27:14 +0300

    tamerlan. Баязид обыгрывает Незнайку.

---
+++ Chess/src/tamerlan/playres/Bayezid.java
@@ -0,0 +1,142 @@
+package tamerlan.playres;
+
+import java.util.Comparator;
+import java.util.List;
+
+import game.core.Board;
+import game.core.GameOver;
+import game.core.GameResult;
+import game.core.Move;
+import game.core.Piece;
+import game.core.PieceColor;
+import game.core.Square;
+import game.core.moves.ICaptureMove;
+import game.core.moves.ITransferMove;
+import game.players.MovePiecePlayer;
+import tamerlan.pieces.King;
+
+/**
+ * Баязид - турецкий султан.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class Bayezid extends MovePiecePlayer{
+	private Comparator<? super Move> movesSorter = new BayezidBrain();
+
+	@Override
+	public String getName() {
+		return "Баязид (Турция)";
+	}
+
+	@Override
+	public String getAuthorName() {
+		return "Романов В.Ю.";
+	}
+
+	@Override
+	public void doMove(Board board, PieceColor color) throws GameOver {
+		List<Move> correctMoves = getCorrectMoves(board, color);
+		
+//		if (correctMoves.isEmpty()) // Пат.
+//			throw new GameOver(GameResult.DRAWN);
+		
+		if (correctMoves.isEmpty())
+			return;
+
+		correctMoves.sort(movesSorter);
+		Move bestMove = correctMoves.get(0);
+		
+		try { bestMove.doMove(); } 
+		catch (GameOver e) {
+			// Сохраняем в истории игры последний сделанный ход 
+			// и результат игры.
+			board.history.addMove(bestMove);
+			board.history.setResult(e.result);
+			
+			// Просим обозревателей доски показать 
+			// положение на доске, сделанный ход и 
+			// результат игры.
+			board.setBoardChanged();
+			
+			// Распространяем инфрмацию об окончании игры.
+			throw new GameOver(e.result);
+		}
+		
+		// Сохраняем ход в истории игры.
+		board.history.addMove(bestMove);
+
+		// Просим обозревателей доски показать 
+		// положение на доске, сделанный ход и 
+		// результат игры.
+		board.setBoardChanged();
+	
+		// Для отладки ограничим количество ходов в игре.
+		// После этого результат игры ничья.
+		if (board.history.getMoves().size() > 80) {
+			// Сохраняем в истории игры последний сделанный ход 
+			// и результат игры.
+			board.history.setResult(GameResult.DRAWN);
+			
+			// Сообщаем что игра закончилась ничьей.
+			throw new GameOver(GameResult.DRAWN);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+}
+
+
+/**
+ * Алгоритм определения лучших ходов у турецкого султана Баязида.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+class BayezidBrain extends PlayerBrain implements Comparator<Move> {
+	@Override
+	public int compare(Move m1, Move m2) {
+		int w1 = getMoveWeight(m1);
+		int w2 = getMoveWeight(m2);
+		
+		return w2 - w1;
+	}
+
+	/**
+	 * Задать вес для хода.
+	 * @param move - ход
+	 * @return оценка хода.
+	 */
+	private int getMoveWeight(Move move) {
+		ITransferMove transfer = (ITransferMove) move;
+		
+		Square source = transfer.getSource();
+		Square target = transfer.getTarget();
+		Piece thePiece = source.getPiece();
+
+		if (move instanceof ICaptureMove) {
+			// Ход - взятие фигуры врага.
+			ICaptureMove capture = (ICaptureMove) move;
+			
+			Square capturedSquare = capture.getCaptured().get(0);
+			Piece  capturedPiece  = capturedSquare.getPiece();
+			
+			// У захвата короля врага наивысший приоритет.
+			if (capturedPiece instanceof King)
+				return 1000;
+			
+			// Пока берем любую фигуру.
+			return 999;
+		}
+		
+		// Из всех ходов без взятия фигуры врага лучший ход
+		// который максимально приближает к королю врага.
+		King enemyKing = getEnemyKing(thePiece);
+		int stepWeight = MAX_DISTANCE - distance(target, enemyKing.square);
+		
+		return stepWeight; 
+//		return getSquareWeight(target);
+	}
+}
+
---
+++ Chess/src/tamerlan/playres/PlayerBrain.java
@@ -0,0 +1,90 @@
+package tamerlan.playres;
+
+import game.core.Board;
+import game.core.Piece;
+import game.core.Square;
+import tamerlan.pieces.Bishop;
+import tamerlan.pieces.Giraffe;
+import tamerlan.pieces.King;
+import tamerlan.pieces.Knight;
+import tamerlan.pieces.Pawn;
+import tamerlan.pieces.Queen;
+import tamerlan.pieces.Rook;
+import tamerlan.pieces.Vizir;
+import tamerlan.pieces.WarMachine;
+
+/**
+ * Базовый класс для алгоритмов выбора лучшего хода.
+ * 
+ * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+ */
+public class PlayerBrain {
+	/**
+	 * Максимальное расстояние между клетками доски.
+	 */
+	protected static final int MAX_DISTANCE = 20;
+
+	/**
+	 * Ценность (вес) поля на доске.
+	 * Чем ближе поле к центру, том оно лучше. 
+	 * @param s - поле
+	 * @return вес поля.
+	 */
+	static
+	public  int getSquareWeight(final Square s) {
+		Board board = s.getBoard();
+		final int tv = s.v;
+		final int th = s.h;
+		
+		final double dv = Math.abs(tv - 0.5 * (board.nV-1) );
+		final double dh = Math.abs(th - 0.5 * (board.nH-1) );
+		int distance = (int) (dv + dh);
+		
+		return MAX_DISTANCE - distance;
+	}
+
+	/**
+	 * Найти короля у фигур - врагов для фигуры piece. 
+	 * @param piece - фигура для которой ищем врага-короля.
+	 * @return вражеский король.
+	 */
+	protected King getEnemyKing(Piece piece) {
+		King enemyKing = null;
+		for (Piece p : piece.getEnemies())
+			if (p instanceof King) {
+				enemyKing = (King) p;
+				break;
+			}
+		return enemyKing;
+	}
+
+	/**
+	 * Выдать расстояние между клетками.
+	 * @param s1 
+	 * @param s2
+	 * @return
+	 */
+	protected int distance(Square s1, Square s2) {
+		final double dv = Math.abs(s1.v - s2.v);
+		final double dh = Math.abs(s1.h - s2.h);
+		return (int) (dv + dh);
+	}
+
+	/**
+	 * Получить вес фигуры в шахматах Тамерлана.
+	 * @param piece фигура в шахматах Тамерлана.
+	 * @return вес фигуры.
+	 */
+	public int getPieceWeight(Piece piece) {
+		if (piece instanceof Pawn)       return 30+1;
+		if (piece instanceof Knight)     return 30+2;
+		if (piece instanceof Bishop)     return 30+3;
+		if (piece instanceof Rook)       return 30+4;
+		if (piece instanceof Vizir)      return 30+5;
+		if (piece instanceof Queen)      return 30+6;
+		if (piece instanceof WarMachine) return 30+7;
+		if (piece instanceof Giraffe)    return 30+8;
+	
+		return 0;
+	}
+}
