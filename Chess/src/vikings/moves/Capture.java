8ee4995de0d81c33a8cce55e3639245065cc8c7e
merkulovas <merkulov.artem.s@gmail.com>
2017-11-23 00:02:26 +0300

    Попытка создания правил Vikings

--- Chess/src/vikings/moves/Capture.java
+++ Chess/src/vikings/moves/Capture.java
@@ -1,64 +1,76 @@
 package vikings.moves;
-
+ 
 import java.util.List;
-
+import game.core.Board;
+import game.core.GameOver;
+import game.core.GameResult;
+import game.core.Piece;
 import game.core.Square;
 import game.core.moves.ICaptureMove;
+import vikings.pieces.VikingsPiece;
+import vikings.pieces.Сyning;
 
-/**
- * Ход с захватом фигуры противника.
- *  
- * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
+/*
+ПОБЕДА БЕЛЫХ
+1.     Если им удаётся поставить короля на открытую прямую к одной из таких клеток, они объявляют «Raichi» (шах), если сразу на две прямые — Tuichi (мат).
+ 
+ПОБЕДА ЧЕРНЫХ
+1.     Король считается захваченным, когда его окружают с четырёх сторон. При этом сторонами могут считаться угловые клетки, трон, и стороны доски. Король может быть захвачен вместе с одной белой фишкой, будучи окружён чёрными со всех сторон.
+2.     Когда королю угрожает опасность быть захваченным следующим ходом, чёрные предупреждают белых (шах королю).
+ 
+УНИЧТОЖЕНИЕ ФИШЕК
+1.     Если фишка своим ходом зажимает фишку противника между собой и другой фишкой или между собой и угловым квадратом, фишка противника считается съеденной. Съедаться может более одной фишки за раз.
+2.     Черные фишки может входить между двумя вражескими, в этом случае она не считается съеденной. При этом белые могут спокойно поставить свою фишку между двумя чёрными.
  */
+
+ 
 public class Capture extends SimpleMove implements ICaptureMove {
-	/**
-	 * Клетка на которых стоят захваченные фигуры.
-	 */
-	private List<Square> captured;
-
-	/**
-	 * Ход с захватом фигур противника.
-	 * 
-	 * @param captured - клетки где стоят захваченные фигуры противника.
-	 * @param squares
-	 *            - клетки хода. 
-	 * 
-	 * <pre>
-	 * squares[0] откуда идет (source) 
-	 * squares[1] куда идет (target)
-	 * </pre>
-	 */
-	public Capture(List<Square> captured, Square[] squares) {
-		super(squares);
-		
-		this.captured = captured;
-	}
-	
-	@Override
-	public List<Square> getCaptured() {
-		return captured;
-	}
-
-	@Override
-	public void doMove() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void undoMove() {
-		// TODO Auto-generated method stub
-	}
-
-	public void removePiece() {
-		// TODO Auto-generated method stub
-	}
-
-	public void restotePiece() {
-		// TODO Auto-generated method stub
-	}
-	
-	@Override
-	public String toString() {
-		return "" + piece + source + "-" + target;
-	}
+           
+            private Piece capturedPiece;
+            private Square capturedSquare;
+           
+            public Capture(List<Square> captured, Square[] squares) {
+                        super(squares);
+                        this.captured = captured;
+            }
+           
+            private List<Square> captured;
+            public List<Square> getCaptured() {
+                        return captured;
+            }
+           
+            public void undoMove() {
+                        super.undoMove();
+                        capturedSquare.setPiece(capturedPiece);
+            }
+           
+            public void doMove() throws GameOver{
+                       
+                        capturedPiece.remove();
+                        super.doMove();
+                        piece.moveTo(target);
+                       
+                        if ((piece instanceof Сyning) && piece.hasNext(null)) {
+                                   throw new GameOver(GameResult.BLACK_WIN);
+                        }
+                       
+        int whitefigure = piece.getFriends().size();
+        int blackfigure = piece.getFriends().size();
+        if (whitefigure == 0) {
+            throw new GameOver(GameResult.WHITE_WIN);
+        }
+        if (blackfigure == 0) {
+            throw new GameOver(GameResult.BLACK_WIN);
+        }
+ 
+ 
+            }
+                       
+            public void removePiece() {}
+ 
+            public void restotePiece() {}
+           
+    public String toString() { return "" + piece + source + "-" + target; }
+ 
 }
+ 
