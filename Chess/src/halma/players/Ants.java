41cf9ba108b1c9c5d66775e07bd6e6a1bdad0d39
V.Y. Romanov <vladimir.romanov@gmail.com>
2017-11-25 00:16:26 +0300

    halma. Фигуры пермещаются в противоположный угол.

--- Chess/src/halma/players/Ants.java
+++ Chess/src/halma/players/Ants.java
@@ -1,5 +1,6 @@
 package halma.players;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -14,13 +15,11 @@ import game.core.moves.ITransferMove;
 import game.players.MovePiecePlayer;
 
 /**
- * Ants - муравьи.
+ * Ants - алгоритм "Муравьи".
  * 
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
-public class Ants extends MovePiecePlayer{
-	private Comparator<? super Move> movesSorter = new AntsBrain();
-
+public class Ants extends MovePiecePlayer {
 	@Override
 	public String getName() {
 		return "Муравьи";
@@ -32,14 +31,32 @@ public class Ants extends MovePiecePlayer{
 	}
 
 	@Override
+	public String toString() {
+		return getName();
+	}
+
+	@Override
 	public void doMove(Board board, PieceColor color) throws GameOver {
 		List<Move> correctMoves = getCorrectMoves(board, color);
 		
 		if (correctMoves.isEmpty())
 			return;
 
-		correctMoves.sort(movesSorter);
-		Move bestMove = correctMoves.get(0);
+		// Случайным образом переставим ходы в списке
+		// что бы не играть всегда одну и ту же игру.
+		Collections.shuffle(correctMoves);
+		
+		// Сначала приоритет ходам с более длинным шагом (на большее число клеток).
+		// Затем среди выбранных с длинным шагом - ход отстающими фигурами.
+		Comparator<ITransferMove> comparator = 
+				maxStep.thenComparing(fromBack).thenComparing(random);
+		
+		Move bestMove = correctMoves
+			.stream()
+			.map(m -> (ITransferMove) m)
+			.sorted(comparator)
+			.findFirst()
+			.get();
 		
 		try { bestMove.doMove(); } 
 		catch (GameOver e) {
@@ -67,7 +84,7 @@ public class Ants extends MovePiecePlayer{
 	
 		// Для отладки ограничим количество ходов в игре.
 		// После этого результат игры ничья.
-		if (board.history.getMoves().size() > 80) {
+		if (board.history.getMoves().size() > 300) {
 			// Сохраняем в истории игры последний сделанный ход 
 			// и результат игры.
 			board.history.setResult(GameResult.DRAWN);
@@ -77,78 +94,89 @@ public class Ants extends MovePiecePlayer{
 		}
 	}
 
-	@Override
-	public String toString() {
-		return getName();
-	}
-}
-
-/**
- * Алгоритм определения лучших ходов у Ants.
- * 
- * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
- */
-class AntsBrain implements Comparator<Move> {
-	@Override
-	public int compare(Move m1, Move m2) {
-		int w1 = getMoveWeight(m1);
-		int w2 = getMoveWeight(m2);
-		
-		return w2 - w1;
-	}
+	/**
+	 * Выбор случайного хода по случайному числу
+	 * в диапазоне от -0.5 до +0.5.
+	 * 
+	 */
+	Comparator<? super ITransferMove> random = 
+		(move1, move2) -> (int)(Math.random() - 0.5);
 
 	/**
-	 * Задать вес для хода.
-	 * @param move - ход
-	 * @return оценка хода.
+	 * Приоритет у хода делающего больший шаг к противоположному углу доски. 
 	 */
-	private int getMoveWeight(Move move) {
-		ITransferMove transfer = (ITransferMove) move;
-		
-		Square source = transfer.getSource();
-		Square target = transfer.getTarget();
-		Piece thePiece = source.getPiece();
+	Comparator<ITransferMove> maxStep = 
+		(move1, move2) -> {
+			// Направление шага.
+			int dir = move1.getPiece().isWhite() ? 1 : -1;
+			
+			int step1 = shift(move1.getSource(), move1.getTarget());
+			int step2 = shift(move2.getSource(), move2.getTarget());
+			
+			return dir * (step2 - step1);
+		};
 
-		Square goal = getPieceGoal(thePiece);
-		
-		return getMaxDistance(target) - distance(goal, target);
-	}
+	/**
+	 * Приоритет у хода с более дальней позиции (отстающими фигурами).</br>
+	 * Тогда будет больше фигур пригодных для "перепрыгивания".
+	 */
+	Comparator<ITransferMove> fromBack = 
+		(move1, move2) -> {
+			Square goal = getPieceGoal(move1.getPiece());
+	
+			// Расстояние до клетки - цели.
+			int distance1 = distance(move1.getSource(), goal);
+			int distance2 = distance(move2.getSource(), goal);
+			
+			return Math.abs(distance2) - Math.abs(distance1);
+	};
 	
 	/**
-	 * Максимальное расстояние между клетками на доске.
-	 *  
-	 * @param square - одна из клеток доски.
-	 * @return
-	 */
-	private int getMaxDistance(Square square) {
-		Board board = square.getBoard();
-		
-		return board.nH + board.nV + 1;
-	}
-
-	/**
-	 * Направление движения для заданной фигуры.
+	 * В сторону какой клетки должна двигаться заданная фигура.
 	 * 
-	 * @param piece - заданная фигура.
-	 * @return в сторону какой клетки двигаться.
+	 * @param piece
+	 *            - задання фигура.
+	 * @return клетка в сторону которой она должна двигаться.
 	 */
 	private Square getPieceGoal(Piece piece) {
 		Board board = piece.square.getBoard();
 
-		return piece.getColor() == PieceColor.BLACK
+		return piece.isBlack()
 				? board.getSquare(0, 0)
 				: board.getSquare(board.nV-1, board.nH-1);
 	}
 
 	/**
-	 * Выдать расстояние между клетками.
-	 * @param s1 
-	 * @param s2
-	 * @return
+	 * Выдать смещение между клетками - сумма смещений по вертикали и горизонтали. 
+	 * Смещение между клетками может быть отрицательным.
+	 * 
+	 * @param source
+	 *            - клетка откуда идет фигура.
+	 * @param target
+	 *            - клетка куда идет фигура.
+	 * @return смещение между клетками target и source.
 	 */
-	protected int distance(Square s1, Square s2) {
-		final double dv = Math.abs(s1.v - s2.v);
-		final double dh = Math.abs(s1.h - s2.h);
-		return (int) (dv + dh);
+	public int shift(Square source, Square target) {
+		int dv = target.v - source.v;
+		int dh = target.h - source.h;
+		
+		return dv + dh;
+	}
+	
+	/**
+	 * Выдать расстояние между клетками доски - 
+	 * сумма расстояний по вертикали и горизонтали.
+	 * 
+	 * @param source
+	 *            - клетка откуда идет фигура.
+	 * @param target
+	 *            - клетка куда идет фигура.
+	 * @return Расстояние между клетками <b>target</b> и <b>source</b>.
+	 */
+	public int distance(Square source, Square target) {
+		int dv = target.v - source.v;
+		int dh = target.h - source.h;
+		
+		return Math.abs(dv) + Math.abs(dh);
 	}
 }
