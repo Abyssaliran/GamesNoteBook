e9efc1cda3e6e38da550f83453f72226cf74a51b
V.Y. Romanov <vladimir.romanov@gmail.com>
2019-10-14 08:01:31 +0300

    Разделили игры и инструменты для игр.

--- Chess/src/game/db/resource/PGNLoader.java
+++
@@ -1,8 +0,0 @@
-package game.db.resource;
-
-/**
- * Класс для загрузки в базу данных файлов в формате PGN.
- */
-public class PGNLoader {
-
-}
---
+++ Chess/src/tools/tourney/RoundTourney.java
@@ -0,0 +1,19 @@
+package tools.tourney;
+
+import game.players.IPlayer;
+
+import java.util.List;
+
+/**
+ * Турнир по круговой системе.
+ */
+public class RoundTourney extends Competition {
+    public RoundTourney(List<IPlayer> players) {
+        super(players);
+    }
+
+    @Override
+    void run() {
+
+    }
+}
