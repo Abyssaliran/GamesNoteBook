22652a499e85343cb871e76f3ad2a27d8b00e4b3
shuluspa <shuluspa@localhost>
2019-11-01 18:27:54 +0300

    Добавлен шаблон для итоговой таблицы

--- Chess/src/game/tourney/ui/RoundTourneyPanel.java
+++ Chess/src/game/tourney/ui/RoundTourneyPanel.java
@@ -34,6 +34,7 @@ class RoundTourneyPanel extends Composite {
         setLayout(new RowLayout(SWT.VERTICAL));
 
         new GamesTable(this, tourney.players);
+        new GamesTableResults(this, tourney.players);
 
         Button start = new Button(this, SWT.PUSH | SWT.CENTER);
         start.setText("Старт");
@@ -68,8 +69,9 @@ class RoundTourneyPanel extends Composite {
      * Ячейка для отображения двух игр для каждого игрока: белыми и черными.
      */
     class GameCell extends Composite {
-        private final Label game1;
-        private final Label game2;
+        private  Label game1;
+        private  Label game2;
+        private  Label game;
 
         GameCell(Composite parent, IPlayer player1, IPlayer player2) {
             super(parent, SWT.BORDER);
@@ -92,6 +94,24 @@ class RoundTourneyPanel extends Composite {
             game2 = new Label(this, SWT.CENTER);
             game2.setText("*");
             game2.setToolTipText(player2.getName() + " - " + player1.getName());
+        }
+        GameCell(Composite parent, IPlayer player) {
+            super(parent, SWT.BORDER);
+            setForeground(COLOR_BLACK);
+            setBackground(COLOR_WHITE);
+
+            GridLayout layout = new GridLayout(1, true);
+            layout.verticalSpacing = 0;
+            setLayout(layout);
+
+            GridData gridData = new GridData();
+            gridData.widthHint = CELL_SIZE;
+            gridData.heightHint = CELL_SIZE;
+            setLayoutData(gridData);
+
+            game = new Label(this, SWT.CENTER);
+            game.setText("*");
+            game.setToolTipText(player.getName());
         }
     }
 
@@ -145,8 +165,54 @@ class RoundTourneyPanel extends Composite {
                 }
             }
         }
+    }
+    
+    /**
+     * Таблица для отображения результатов соревнования проводимого по круговой системе.
+     */
+    public class GamesTableResults extends Composite {
+        GamesTableResults(Composite parent, List<IPlayer> players) {
+            super(parent, SWT.NONE);
 
-        // TODO Шулус: для игрока добавить колонки колонки сколько очков, сколько выиграл, проиграл и ничьи.
+            int nPlayers = players.size();
+
+            GridLayout layout = new GridLayout(1 + 1, false);
+            layout.horizontalSpacing = 0;
+            layout.verticalSpacing = 0;
+            setLayout(layout);
+            
+            // Верхний левый угол таблицы.
+            new Label(this, SWT.NONE);
+
+
+                Label playersScore = new Label(this, SWT.CENTER);
+                playersScore.setForeground(COLOR_WHITE);
+                playersScore.setText("Очки");
+                //playersScore.setToolTipText(player.getName());
+               
+
+            // Строки таблицы с результатами игры
+            // для игрока записанногоо в начале строки.
+            for (int k = 0; k < nPlayers; k++) {
+                IPlayer player = players.get(k);
+
+                // Номер игрока в таблице и имя игрока.
+                String txt = String.format("%2d. %s", 1 + k, player.getName());
+
+                Label name = new Label(this, SWT.LEFT);
+                name.setForeground(COLOR_WHITE);
+                name.setText(txt + " ");
+                name.setToolTipText("Автор алгоритма: " + player.getAuthorName());
+
+                
+                for (int i = 0; i<1; i++) {
+                    //boolean isDiagonal = (player == opponent);
+
+                    if (/*isDiagonal*/ false) new EmptyCell(this);
+                    else new GameCell(this, player);
+                }
+            }
+        }
     }
 }
 
