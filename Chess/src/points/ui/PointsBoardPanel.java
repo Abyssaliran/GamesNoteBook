fce452b04a5a88985ea8b7d39e65336f0f9de059
V.Y. Romanov <vladimir.romanov@gmail.com>
2019-11-05 15:31:04 +0300

    TODO Алексей Голосов. Проверить завершение игры.

--- Chess/src/points/ui/PointsBoardPanel.java
+++ Chess/src/points/ui/PointsBoardPanel.java
@@ -11,7 +11,7 @@ import game.core.Square;
 import game.ui.AsiaBoard;
 import game.ui.listeners.PutPieceListener;
 import game.ui.listeners.PutPiecePromptListener;
-import renju.piece.Stone;
+import points.pieces.Stone;
 import reversi.ui.images.ReversiImages;
 
 public class PointsBoardPanel extends AsiaBoard implements IPieceProvider  {
