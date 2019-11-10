9ac6d6a98855dc5286af4ed8177578f378e2fd8d
Andrey Bagrov <andrey.bagrov@yandex.ru>
2019-11-10 04:31:24 +0300

    Добавлен новый игрок - Карабас Умный.

--- Chess/src/renju/Renju.java
+++ Chess/src/renju/Renju.java
@@ -8,6 +8,7 @@ import game.players.Vinni;
 import renju.pieces.Stone;
 import renju.players.Buratino;
 import renju.players.Karabas;
+import renju.players.KarabasClever;
 
 public class Renju extends Game {
 	private static final int BOARD_SIZE = 15;
@@ -19,6 +20,7 @@ public class Renju extends Game {
 		Game.addPlayer(Renju.class, new Vinni(pieceProvider, BOARD_SIZE * BOARD_SIZE));
 		Game.addPlayer(Renju.class, new Buratino(pieceProvider));
 		Game.addPlayer(Renju.class, new Karabas(pieceProvider));
+		Game.addPlayer(Renju.class, new KarabasClever(pieceProvider));
 	}
 
 	public Renju() {
