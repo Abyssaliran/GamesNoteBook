104dc26b72d7b17e8c7e757a9b8551529376e501
romankhozin <romankhozin@192.168.1.165>
2019-11-17 20:36:59 +0300

    Добавлено поле isTransformable и transformSquare

--- Chess/src/shogi/pieces/ShogiPiece.java
+++ Chess/src/shogi/pieces/ShogiPiece.java
@@ -5,11 +5,15 @@ import game.core.Piece;
 import game.core.PieceColor;
 import game.core.Square;
 import shogi.moves.Capture;
+import shogi.moves.Promotion;
 import shogi.moves.SimpleMove;
 
 abstract
 public class ShogiPiece extends Piece {
-	public boolean isTransformaed = false;
+	public boolean isTransformed = false; //Трансформирована ли фигура в данный момент
+	public boolean isTransformable = false; //Трансформируема ли фигура
+	
+	public Square transformSquare;
 
 	public ShogiPiece(Square square, PieceColor color) {
 		super(square, color);
@@ -31,8 +35,9 @@ public class ShogiPiece extends Piece {
 	public Move makeMove(Square... squares) {
 		Square source = squares[0];
 		Square target = squares[1];
-
+		
 		return target.isEmpty() ? new SimpleMove(this, source, target)
 				                : new Capture(squares);
+		
 	}
 }
