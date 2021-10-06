20ecc528d92bbc7b5173944c765468e178b7f4bc
Vladimir Y.  Romanov <vladimir.romanov@gmail.com>
2021-10-06 16:37:57 +0300

    TODO распознать ход по тексту, проверить его корректность и вернуть его как результат функции.

--- Chess/src/chess/pgn/PNGReader.java
+++ Chess/src/chess/pgn/PNGReader.java
@@ -1,60 +1,133 @@
 package chess.pgn;
 
+import static game.core.GamesConfig.pngRoot;
+
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.Reader;
 import java.io.StreamTokenizer;
 import java.util.ArrayList;
 import java.util.List;
 
 import chess.Chess;
+import game.core.Move;
 
 /**
  * Класс для чтения игр из текстового файла в стандартной шахматной нотации
  * Portable Game Notation (PGN).
+ * 
+ * https://docs.oracle.com/javase/7/docs/api/java/io/StreamTokenizer.html
  */
 public class PNGReader {
-	/**
-	 * @param fileName файл с расширением *.pgn
-	 * @return список партий из шахмат.
-	 */
-	static public List<Chess> read(String fileName) {
-		List<Chess> games = new ArrayList<Chess>();
+	static PrintStream out = System.out;
 
+	public static void main(String[] args) {
 		try {
-			FileReader fr = new FileReader(fileName);
-			Reader br = new BufferedReader(fr);
-
-			StreamTokenizer streamTokenizer = new StreamTokenizer(br);
-
-			List<Object> tokens = new ArrayList<Object>();
-
-			int currentToken = streamTokenizer.nextToken();
-			while (currentToken != StreamTokenizer.TT_EOF) {
-				switch (streamTokenizer.ttype) {
-				case '[':
-					readTag(streamTokenizer);
-					break;
-
-				case StreamTokenizer.TT_NUMBER:
-					streamTokenizer.pushBack();
-					readMoves(streamTokenizer);
-					break;
-
-				default:
-					tokens.add((char) currentToken);
-					break;
-				}
-
-				currentToken = streamTokenizer.nextToken();
-			}
+			read(pngRoot + "/Carlsen.pgn");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
+	/**
+	 * @param fileName файл с расширением *.pgn
+	 * @return список партий из шахмат.
+	 * @throws FileNotFoundException
+	 */
+	static public List<Chess> read(String fileName) throws IOException {
+		out = new PrintStream(fileName + ".txt");
+
+		FileReader fr = new FileReader(fileName);
+		Reader br = new BufferedReader(fr);
+
+		StreamTokenizer streamTokenizer = new StreamTokenizer(br);
+		streamTokenizer.slashSlashComments(false);
+		streamTokenizer.slashStarComments(false);
+
+		// Символ / - это обычный символ (это не начало комментария).
+		streamTokenizer.ordinaryChar('/');
+
+		// Символ - - это обычный символ (это не минус перед числом).
+		streamTokenizer.ordinaryChar('-');
+		streamTokenizer.ordinaryChar('+');
+
+		streamTokenizer.wordChars('-', '-');
+		streamTokenizer.wordChars('+', '+');
+		streamTokenizer.wordChars('=', '=');
+
+		List<Chess> games = new ArrayList<Chess>();
+
+		Chess chess = new Chess();
+		while (readGame(streamTokenizer, chess)) {
+			games.add(chess);
+			chess = new Chess();
+		}
+
+		out.close();
 		return games;
+	}
+
+	private static boolean readGame(StreamTokenizer streamTokenizer, Chess chess) throws IOException {
+		int currentToken = streamTokenizer.nextToken();
+		while (currentToken != StreamTokenizer.TT_EOF) {
+			switch (streamTokenizer.ttype) {
+			case '[':
+				readTag(streamTokenizer, chess);
+				break;
+
+			case StreamTokenizer.TT_NUMBER:
+				streamTokenizer.pushBack();
+				readMoves(streamTokenizer, chess);
+				return true;
+			}
+
+			currentToken = streamTokenizer.nextToken();
+		}
+		return false;
+	}
+
+	private static void readMoves(StreamTokenizer streamTokenizer, Chess chess) throws IOException {
+		int currentToken = streamTokenizer.nextToken();
+
+		while (currentToken != StreamTokenizer.TT_EOF) {
+			switch (streamTokenizer.ttype) {
+			case '[':
+				streamTokenizer.pushBack();
+				out.println();
+				return;
+
+			case StreamTokenizer.TT_WORD:
+				Move move = getChessMove(chess, streamTokenizer.sval);
+				chess.board.history.addMove(move);
+				break;
+
+			case StreamTokenizer.TT_NUMBER:
+				if (isGameResult(streamTokenizer)) {
+					out.println();
+					out.println();
+					return;
+				}
+				out.print("\n" + (int) streamTokenizer.nval + ".");
+				break;
+
+			default:
+				out.print((char) currentToken);
+				break;
+			}
+
+			currentToken = streamTokenizer.nextToken();
+		}
+	}
+
+	private static Move getChessMove(Chess chess, String moveText) {
+		out.print(" " + moveText);
+
+		// TODO распознать ход по тексту, проверить его корректность
+		// и вернуть его как результат функции.
+		return null;
 	}
 
 	private static boolean isGameResult(StreamTokenizer streamTokenizer) throws IOException {
@@ -62,83 +135,55 @@ public class PNGReader {
 
 		switch (streamTokenizer.ttype) {
 		case '-': // 1-0 или 0-1
-			streamTokenizer.nextToken(); // 0 или 1
+			currentToken = streamTokenizer.nextToken(); // 0 или 1
 			return true;
 		case '/': // 1/2-1/2
-			streamTokenizer.nextToken(); // 2
-			streamTokenizer.nextToken(); // -
-			streamTokenizer.nextToken(); // 1
-			streamTokenizer.nextToken(); // /
-			streamTokenizer.nextToken(); // 2
+			currentToken = streamTokenizer.nextToken(); // 2
+			currentToken = streamTokenizer.nextToken(); // -
+			currentToken = streamTokenizer.nextToken(); // 1
+			currentToken = streamTokenizer.nextToken(); // /
+			currentToken = streamTokenizer.nextToken(); // 2
+			return true;
+		case StreamTokenizer.TT_NUMBER:
+			currentToken = streamTokenizer.nextToken();  
+			if (!streamTokenizer.sval.equals("-")) {
+				streamTokenizer.pushBack();
+				return false;
+			}
+			currentToken = streamTokenizer.nextToken();  
 			return true;
 		default:
+			if (streamTokenizer.sval.equals("-1") || streamTokenizer.sval.equals("-0"))
+				return true;
 			streamTokenizer.pushBack();
-//			streamTokenizer.pushBack();
 			return false;
 		}
 	}
 
-	private static void readMoves(StreamTokenizer streamTokenizer) throws IOException {
+	static private void readTag(StreamTokenizer streamTokenizer, Chess chess) throws IOException {
 		int currentToken = streamTokenizer.nextToken();
-
-		while (currentToken != StreamTokenizer.TT_EOF) {
-			switch (streamTokenizer.ttype) {
-			case '[':
-				streamTokenizer.pushBack();
-				System.out.println();
-				return;
-
-			case StreamTokenizer.TT_WORD:
-				System.out.print(" " + streamTokenizer.sval);
-				break;
-
-			case StreamTokenizer.TT_NUMBER:
-				if (isGameResult(streamTokenizer))  
-					return;
-				System.out.print("\n" + (int) streamTokenizer.nval + ".");
-				break;
-
-			default:
-				System.out.print((char) currentToken);
-				break;
-			}
-
-			currentToken = streamTokenizer.nextToken();
-		}
-	}
-
-	static private void readTag(StreamTokenizer streamTokenizer) throws IOException {
-		int currentToken = streamTokenizer.nextToken();
+		String tagName = null, tagValue;
 
 		while (currentToken != StreamTokenizer.TT_EOF) {
 			switch (streamTokenizer.ttype) {
 			case ']':
-				System.out.println();
+				out.println();
 				return;
 
 			case StreamTokenizer.TT_WORD:
-				System.out.print(streamTokenizer.sval + "=");
+				tagName = streamTokenizer.sval;
+				out.print(streamTokenizer.sval + "=");
 				break;
 
 			default:
-				System.out.print(streamTokenizer.sval);
+				tagValue = streamTokenizer.sval;
+				out.print(tagValue);
+				chess.tags.put(tagName, tagValue);
 				break;
 			}
 
 			currentToken = streamTokenizer.nextToken();
 		}
-
 	}
 
-	public static void main(String[] args) {
-		File f = new File(".");
-		String root = f.getAbsolutePath();
-		System.out.println("Project root: " + root);
-
-		String png = root.replace(".", "png/");
-		System.out.println("PGN root: " + png);
-		System.out.println();
-
-		read(png + "Carlsen.pgn");
-	}
 }
