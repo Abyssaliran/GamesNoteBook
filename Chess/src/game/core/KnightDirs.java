ec76ed9ee39c33655336e76d0959320adf4f00b6
Vladimir Y.  Romanov <vladimir.romanov@gmail.com>
2021-10-06 21:20:20 +0300

    KnightDirs для упрощения поиска хода коня.

--- Chess/src/game/core/KnightDirs.java
+++ Chess/src/game/core/KnightDirs.java
@@ -6,17 +6,17 @@ package game.core;
  * @author <a href="mailto:vladimir.romanov@gmail.com">Romanov V.Y.</a>
  */
 public enum KnightDirs {
-	LEFT_UP1(-1, +2), RIGHT_UP1  (+2, +1),
-	LEFT_UP2(-2, +1), RIGHT_UP2  (+1, +2),
-	LEFT_UP3(-2, -1), RIGHT_UP3  (-1, +2),
-	LEFT_UP4(-1, -2), RIGHT_UP4  (-2, +1);
+	LEFT_UP1(-1, +1), RIGHT_UP1(+2, +1),
+	LEFT_UP2(-2, +2), RIGHT_UP2(+1, +2),
+	LEFT_DOWN1(-2, -1), RIGHT_DOWN1(-1, +1),
+	LEFT_DOWN2(-1, -2), RIGHT_DOWN2(-2, +2);
 	
 	public static final 
 	KnightDirs[] ALL = {
 		LEFT_UP1, RIGHT_UP1,
 		LEFT_UP2, RIGHT_UP2,
-		LEFT_UP3, RIGHT_UP3,
-		LEFT_UP4, RIGHT_UP4
+		LEFT_DOWN1, RIGHT_DOWN1,
+		LEFT_DOWN2, RIGHT_DOWN2
 	};
 	
 	/**
