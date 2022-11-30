ce36e84e98b298aab933ad4a163f03a870486339
Vladimir Y.  Romanov <vladimir.romanov@gmail.com>
2022-11-30 11:55:45 +0300

    TODO Пришлецов Сергей

--- Chess/src/breakthrough/players/Aesculapius.java
+++ Chess/src/breakthrough/players/Aesculapius.java
@@ -3,6 +3,7 @@ package breakthrough.players;
 import game.core.Move;
 
 /**
+ * TODO Пришлецов Сергей
  * <a href=
  * "https://ru.wikipedia.org/wiki/%D0%90%D1%81%D0%BA%D0%BB%D0%B5%D0%BF%D0%B8%D0%B9">
  * Аскле́пий </a>
@@ -15,7 +16,7 @@ public class Aesculapius extends BreakThroughPlayer {
 
 	@Override
 	public String getAuthorName() {
-		return "???";
+		return "Пришлецов Сергей";
 	}
 
 	protected int getWeight(Move m2) {
