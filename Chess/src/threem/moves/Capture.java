6d5a36719eb835d565cdc540519795be2f8cc4ab
Petrova Anastasia <petrovaanastasiax@gmail.com>
2022-12-28 14:10:31 +0300

    feat: added win conditions for musketeers and enemies

--- Chess/src/threem/moves/Capture.java
+++ Chess/src/threem/moves/Capture.java
@@ -3,30 +3,62 @@ package threem.moves;
 import game.core.GameOver;
 import game.core.Piece;
 import game.core.Square;
+import game.core.Board;
+import game.core.PieceColor;
+import game.core.GameResult;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Capture extends SimpleMove {
-	private Piece captured;
+    private Piece captured;
 
-	public Capture(Square[] squares) {
-		super(squares);
+    public Capture(Square[] squares) {
+        super(squares);
 
-		captured = target.getPiece();
-	}
+        captured = target.getPiece();
+    }
 
-	@Override
-	public void doMove() throws GameOver {
-		captured.remove();
-		super.doMove();
-	}
+    private void checkGameFinished(Board board) throws GameOver {
+        List<Piece> musketeers = board.getPieces(PieceColor.BLACK);
+        List<Integer> enemiesNearMusketeer = new LinkedList<>();
 
-	@Override
-	public void undoMove() {
-		super.undoMove();
-		target.setPiece(captured);
-	}
+        for (Piece musketeer : musketeers) {
+            List<Piece> enemies = board.getPieces(PieceColor.WHITE);
+            enemies = enemies.stream()
+                    .filter(it -> musketeer.square.isNear(it.square))
+                    .filter(it -> musketeer.square.isVertical(it.square) || musketeer.square.isHorizontal(it.square))
+                    .collect(Collectors.toList());
+            enemiesNearMusketeer.add(enemies.size());
+        }
 
-	@Override
-	public String toString() {
-		return "" + piece + source + "x" + target;
-	}
+        int nonZeroElements = 0;
+        for (Integer count : enemiesNearMusketeer) {
+            if (count != 0) {
+                nonZeroElements++;
+            }
+        }
+        if (nonZeroElements == 0) {
+            throw new GameOver(GameResult.BLACK_WIN);
+        }
+    }
+
+    @Override
+    public void doMove() throws GameOver {
+        captured.remove();
+        super.doMove();
+        checkGameFinished(target.getBoard());
+    }
+
+    @Override
+    public void undoMove() {
+        super.undoMove();
+        target.setPiece(captured);
+    }
+
+    @Override
+    public String toString() {
+        return "" + piece + source + "x" + target;
+    }
 }
--- Chess/src/threem/moves/SimpleMove.java
+++ Chess/src/threem/moves/SimpleMove.java
@@ -4,6 +4,13 @@ import game.core.GameOver;
 import game.core.Piece;
 import game.core.Square;
 import game.core.moves.ITransferMove;
+import game.core.Board;
+import game.core.PieceColor;
+import game.core.GameResult;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SimpleMove implements ITransferMove {
 	public Square source;
@@ -22,9 +29,29 @@ public class SimpleMove implements ITransferMove {
 		return piece;
 	}
 
+	private void checkGameFinished(Board board) throws GameOver {
+		List<Piece> musketeers = board.getPieces(PieceColor.BLACK);
+
+		Map<Integer, Integer> horizontalCounter = new HashMap<>();
+		Map<Integer, Integer> verticalCounter = new HashMap<>();
+
+		for (Piece piece : musketeers) {
+			int h = piece.square.h;
+			horizontalCounter.merge(h, 1, Integer::sum);
+
+			int v = piece.square.v;
+			verticalCounter.merge(v, 1, Integer::sum);
+		}
+
+		if (verticalCounter.containsValue(3) || horizontalCounter.containsValue(3)) {
+			throw new GameOver(GameResult.WHITE_WIN);
+		}
+	}
+
 	@Override
 	public void doMove() throws GameOver {
 		piece.moveTo(target);
+		checkGameFinished(target.getBoard());
 	}
 
 	@Override
